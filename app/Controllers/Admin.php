b023741f34aca4361c97cbdbbeba66c566fe2ffa
yuwandianto <yuwandianto@gmail.com>
2023-02-14 15:57:54 +0800

    fixed

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -694,25 +694,25 @@ class Admin extends BaseController
     $curl = curl_init();
 
     curl_setopt_array($curl, array(
-    CURLOPT_URL => 'http://mywapi.guru-muda.com/kirim-pesan',
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => 'POST',
-    CURLOPT_POSTFIELDS => array(
-        'message' => $pesan,
-        'number' => $data->phone,
-        'token' => 'TokenSaya'),
+        CURLOPT_URL => $api,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => array(
+            'message' => $pesan,
+            'number' => $data->phone,
+            'token' => 'TokenSaya'
+        ),
     ));
 
     $response = curl_exec($curl);
 
     curl_close($curl);
     echo $response;
-
 
     }
 }
