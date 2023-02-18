2a5bbe69e7094c69de7f9c31ba48dcd46f617f63
yuwandianto <yuwandianto@gmail.com>
2023-02-18 22:51:48 +0800

    update

--- app/Views/admin/pages/users.php
+++ app/Views/admin/pages/users.php
@@ -2,7 +2,6 @@
 
 <?= $this->section('content') ?>
 <div class="container-fluid py-4">
-
     <div class="row">
         <div class="col-12">
             <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#modalTambahPengguna">Tambah User</button>
@@ -39,105 +38,94 @@
                         </table>
                     </div>
                 </div>
-
             </div>
-
-
         </div>
     </div>
-
-
-
 </div>
 
-
 <div class="modal fade" id="modalTambahPengguna" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title font-weight-normal" id="exampleModalLabel">Tambah data pengguna</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title font-weight-normal" id="exampleModalLabel">Tambah data pengguna</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="row mb-2">
+                    <div class="input-group input-group-outline">
+                        <label for="" class="col-4">Email Login</label>
+                        <input type="email" name="emailUser" id="emailUser" required class="form-control" placeholder="Masukkan Email Login User" >
+                    </div>
                 </div>
-               
-                <div class="modal-body">
-                    <div class="row mb-2">
-                        <div class="input-group input-group-outline">
-                            <label for="" class="col-4">Email Login</label>
-                            <input type="email" name="emailUser" id="emailUser" required class="form-control" placeholder="Masukkan Email Login User" >
-                        </div>
+                <div class="row mb-2">
+                    <div class="input-group input-group-outline">
+                        <label for="" class="col-4">Nama Pengguna</label>
+                        <input type="text"  name="namaUser" id="namaUser" required class="form-control" placeholder="Masukkan nama user" >
                     </div>
-                    <div class="row mb-2">
-                        <div class="input-group input-group-outline">
-                            <label for="" class="col-4">Nama Pengguna</label>
-                            <input type="text"  name="namaUser" id="namaUser" required class="form-control" placeholder="Masukkan nama user" >
-                        </div>
-                    </div>
-                    <div class="row mb-2">
-                        <div class="input-group input-group-outline">
-                            <label for="" class="col-4">Nomor HP</label>
-                            <input type="number"  name="noHP" id="noHP" required class="form-control" placeholder="Masukkan nomor hp (jika ada)" >
-                        </div>
-                    </div>
-                    <div class="row mb-2">
-                        <div class="input-group input-group-outline">
-                            <label for="" class="col-4">Password Login</label>
-                            <input type="text"  name="passwordUser" id="passwordUser" class="form-control" placeholder="Password Login">
-                        </div>
-                    </div>
-                   
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn bg-gradient-warning" data-bs-dismiss="modal">Tutup</button>
-                    <button type="submit" id="tombolSimpanUser" class="btn bg-gradient-success">Simpan</button>
+                <div class="row mb-2">
+                    <div class="input-group input-group-outline">
+                        <label for="" class="col-4">Nomor HP</label>
+                        <input type="number"  name="noHP" id="noHP" required class="form-control" placeholder="Masukkan nomor hp (jika ada)" >
+                    </div>
                 </div>
-             
+                <div class="row mb-2">
+                    <div class="input-group input-group-outline">
+                        <label for="" class="col-4">Password Login</label>
+                        <input type="text"  name="passwordUser" id="passwordUser" class="form-control" placeholder="Password Login">
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn bg-gradient-warning" data-bs-dismiss="modal">Tutup</button>
+                <button type="submit" id="tombolSimpanUser" class="btn bg-gradient-success">Simpan</button>
             </div>
         </div>
+    </div>
 </div>
 
 <div class="modal fade" id="modalUbahPengguna" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title font-weight-normal" id="exampleModalLabel">Tambah data pengguna</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-               <?= form_open('admin/prosesUbahUser') ;?>
-               <input type="hidden" name="idUser" id="idUser">
-                <div class="modal-body">
-                    <div class="row mb-2">
-                        <div class="input-group input-group-outline">
-                            <label for="" class="col-4">Email Login</label>
-                            <input type="email" name="u_emailUser" id="u_emailUser" required class="form-control" placeholder="Masukkan Email Login User" >
-                        </div>
-                    </div>
-                    <div class="row mb-2">
-                        <div class="input-group input-group-outline">
-                            <label for="" class="col-4">Nama Pengguna</label>
-                            <input type="text"  name="u_namaUser" id="u_namaUser" required class="form-control" placeholder="Masukkan nama user" >
-                        </div>
-                    </div>
-                    <div class="row mb-2">
-                        <div class="input-group input-group-outline">
-                            <label for="" class="col-4">Nomor HP</label>
-                            <input type="number"  name="u_nomorHP" id="u_nomorHP" class="form-control" placeholder="Masukkan nomor hp (jika ada)" >
-                        </div>
-                    </div>
-                    <div class="row mb-2">
-                        <div class="input-group input-group-outline">
-                            <label for="" class="col-4">Password Login</label>
-                            <input type="text"  name="u_passwordUser" id="u_passwordUser" class="form-control" placeholder="Password Login">
-                        </div>
-                    </div>
-                   
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn bg-gradient-warning" data-bs-dismiss="modal">Tutup</button>
-                    <button type="submit" id="tombolSimpanUser" class="btn bg-gradient-success">Simpan</button>
-                </div>
-             <?= form_close() ;?>
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title font-weight-normal" id="exampleModalLabel">Tambah data pengguna</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
+            <?= form_open('admin/prosesUbahUser') ;?>
+            <input type="hidden" name="idUser" id="idUser">
+            <div class="modal-body">
+                <div class="row mb-2">
+                    <div class="input-group input-group-outline">
+                        <label for="" class="col-4">Email Login</label>
+                        <input type="email" name="u_emailUser" id="u_emailUser" required class="form-control" placeholder="Masukkan Email Login User" >
+                    </div>
+                </div>
+                <div class="row mb-2">
+                    <div class="input-group input-group-outline">
+                        <label for="" class="col-4">Nama Pengguna</label>
+                        <input type="text"  name="u_namaUser" id="u_namaUser" required class="form-control" placeholder="Masukkan nama user" >
+                    </div>
+                </div>
+                <div class="row mb-2">
+                    <div class="input-group input-group-outline">
+                        <label for="" class="col-4">Nomor HP</label>
+                        <input type="number"  name="u_nomorHP" id="u_nomorHP" class="form-control" placeholder="Masukkan nomor hp (jika ada)" >
+                    </div>
+                </div>
+                <div class="row mb-2">
+                    <div class="input-group input-group-outline">
+                        <label for="" class="col-4">Password Login</label>
+                        <input type="text"  name="u_passwordUser" id="u_passwordUser" class="form-control" placeholder="Password Login">
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn bg-gradient-warning" data-bs-dismiss="modal">Tutup</button>
+                <button type="submit" id="tombolSimpanUser" class="btn bg-gradient-success">Simpan</button>
+            </div>
+            <?= form_close() ;?>
         </div>
+    </div>
 </div>
 
 
